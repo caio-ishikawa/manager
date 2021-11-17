1d21d0a85b958b0e0713ca25108c00dde61cc8f6
caio ishikawa <caioarbishikawa@gmail.com>
2021-11-17 17:07:16 +0000

    gitignore

--- server/secrets.js
+++
@@ -1,5 +0,0 @@
-const router        = require('express').Router();
-
-const secrets = 'mongodb+srv://admin:AdminPassword123@laravel1.q2vv7.mongodb.net/projectManager?retryWrites=true&w=majority';
-
-module.exports = secrets;
